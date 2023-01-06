21b1bd1a524844e36d42d8e964d166ec28d08ed0
mintdart <96025197+mintdart@users.noreply.github.com>
2023-01-06 12:49:51 +0100

    add palette routes

---
+++ app/modules/get-color.ts
@@ -0,0 +1,41 @@
+import Vibrant from "node-vibrant";
+import { shade } from "polished";
+import { hex } from "wcag-contrast";
+
+export const getColor = async (path?: string) => {
+  let color = defaultColor;
+
+  try {
+    if (path) {
+      if (path.match(/\.(jpg|jpeg|png)$/)) {
+        await Vibrant.from(path).getPalette((_err, palette) => {
+          if (palette?.Vibrant) {
+            let detectedHex = palette.Vibrant.hex;
+            let AAscore = hex(detectedHex, "#FFF");
+
+            while (AAscore < 3) {
+              detectedHex = shade(0.005, detectedHex);
+              AAscore = hex(detectedHex, "#FFF");
+            }
+
+            color = detectedHex;
+          }
+        });
+      }
+    }
+  } catch (error) {
+    console.log(`Couldn't get color from ${path}`);
+  } finally {
+    return new Response(color, {
+      headers: {
+        ...(color !== defaultColor && {
+          "Cache-Control": "public, max-age=14400",
+          "CDN-Cache-Control": "public, max-age=31536000",
+        }),
+      },
+      status: 200,
+    });
+  }
+};
+
+export const defaultColor = "#2172E5";
