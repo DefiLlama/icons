f2fc0ec3eb3c7e1e7e10088b79f9cc2fb6bcedcd
mintdart <96025197+mintdart@users.noreply.github.com>
2023-02-05 10:42:28 -0800

    add trust wallet repo backup for token images

--- app/routes/icons/tokens/$.ts
+++ app/routes/icons/tokens/$.ts
@@ -1,6 +1,33 @@
 import type { LoaderArgs } from "@remix-run/node";
+import { getAddress } from "ethers";
 import { extractParams, handleError, streamingResizeBuffer } from "~/modules/image-resize";
 import { resToBuffer } from "~/modules/response";
+
+export const trustWalletChainsMap: { [chainId: number]: string } = {
+  1: "ethereum",
+  56: "smartchain",
+  137: "polygon",
+  10: "optimism",
+  42161: "arbitrum",
+  43114: "avalanchec",
+  100: "xdai",
+  250: "fantom",
+  // 8217: "klaytn",
+  1313161554: "aurora",
+  42220: "celo",
+  25: "cronos",
+  // 2000: "dogechain",
+  // 1285: "moonriver",
+  // 199: "bttc",
+  42262: "oasis",
+  // 106: "velas",
+  128: "heco",
+  1666600000: "harmony",
+  // 288: "boba",
+  66: "okexchain",
+  // 122: "fuse",
+  1284: "moonbeam",
+} as const;
 
 export const loader = async ({ params, request }: LoaderArgs) => {
   // extract all the parameters from the url
@@ -25,17 +52,29 @@ export const loader = async ({ params, request }: LoaderArgs) => {
       throw new Error(`${src}: Couldn't find token`);
     }
 
-    console.log(tokenList.tokens[chainId][tokenAddress]);
-
     // fetch token image
-    const tokenImage = await fetch(tokenList.tokens[chainId][tokenAddress]);
-
-    console.log(tokenImage);
+    let tokenImage = await fetch(tokenList.tokens[chainId][tokenAddress]);
 
     const contentType = tokenImage.headers.get("content-type");
 
     if (!contentType || !contentType.startsWith("image")) {
-      throw new Error(`${src}: Failed to fetch token image`);
+      if (trustWalletChainsMap[Number(chainId)]) {
+        const trustWalletImage = await fetch(
+          `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/${
+            trustWalletChainsMap[Number(chainId)]
+          }/assets/${getAddress(tokenAddress)}/logo.png`,
+        );
+
+        const cType = trustWalletImage.headers.get("content-type");
+
+        if (cType && cType.startsWith("image")) {
+          tokenImage = trustWalletImage;
+        } else {
+          throw new Error(`${src}: Failed to fetch token image`);
+        }
+      } else {
+        throw new Error(`${src}: Failed to fetch token image`);
+      }
     }
 
     const resBuffer = await resToBuffer(tokenImage);
