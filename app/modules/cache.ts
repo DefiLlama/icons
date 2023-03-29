3257a215544a1d64f7430bf3f1e8555efeb948b9
strobie <0xstrobe@protonmail.com>
2023-03-29 12:53:14 +0200

    wip

--- app/modules/cache.ts
+++ app/modules/cache.ts
@@ -5,23 +5,44 @@ config();
 
 export const redis = createClient({ url: process.env.REDIS_URL });
 
-export const setCache = async (key: string, value: Buffer) => {
+export const setCache = async (key: string, value: string | Buffer) => {
   try {
     await redis.set(key, value);
     return true;
   } catch (err) {
+    console.log("error in setCache: " + key);
     console.log(err);
     return false;
   }
 };
 
-export const getCache = async (key: string) => {
+export const getCacheBin = async (key: string) => {
   try {
     const data = await redis.get(key);
     if (!data) return null;
     return Buffer.from(data);
   } catch (err) {
+    console.log("error in getCacheBin: " + key);
     console.log(err);
     return null;
   }
+};
+
+export const getCacheString = async (key: string) => {
+  try {
+    const data = await redis.get(key);
+    return data;
+  } catch (err) {
+    console.log("error in getCacheString: " + key);
+    console.log(err);
+    return null;
+  }
+};
+
+export const withCache = async (key: string, func: Function) => {
+  const data = await getCacheBin(key);
+  if (data) return data;
+  const newData = await func();
+  await setCache(key, newData);
+  return newData;
 };
